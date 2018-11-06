ec84cccf0ec3792f62bdd2785b08586aba29124c
roger yee <ryee.epicodus@gmail.com>
2018-11-06 12:02:03 -0800

    successfully adds form elements

--- src/CreateAuctionForm.tsx
+++ src/CreateAuctionForm.tsx
@@ -1,5 +1,47 @@
 import * as React from "react";
+import { Formik } from "formik";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+interface FormValues {
+  name: string;
+  price: number;
+}
 
 export const CreateAuctionForm = () => {
-  return <div>hi</div>;
+  return (
+    <Formik<FormValues>
+      initialValues={{
+        name: "",
+        price: 0
+      }}
+      onSubmit={({ name, price }) => {
+        // call mutation
+      }}
+    >
+      {({ values, handleChange, handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+          <TextField
+            name="name"
+            label="Name"
+            value={values.name}
+            onChange={handleChange}
+            margin="normal"
+          />
+          <br />
+          <TextField
+            name="price"
+            label="Price"
+            value={values.price}
+            onChange={handleChange}
+            margin="normal"
+          />
+          <br />
+          <Button variant="contained" type="submit">
+            Submit
+          </Button>
+        </form>
+      )}
+    </Formik>
+  );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import AWSAppSyncClient, { AUTH_TYPE } from "aws-appsync";
 import { ApolloProvider } from "react-apollo";
